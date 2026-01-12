2ec99e1a605466e6a943733e1d25760fd38145c8
Mihai Gătejescu <mgatejescu@pm.me>
2026-01-12 13:46:49 +0200

    Add the login and register buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import Online from "./components/Online.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+          <Online/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
-
-export default App
---
+++ src/components/Online.jsx
@@ -0,0 +1,11 @@
+import {Button, Stack} from "@mui/material";
+
+export default function Online() {
+
+    return (
+        <Stack direction="column" spacing={0.5}>
+            <Button variant="contained">Login</Button>
+            <Button variant="contained">Register</Button>
+        </Stack>
+    )
+}
