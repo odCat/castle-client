87b6460a497e05fa52e5a7d53ad0109c4699c5d4
Mihai Gătejescu <mgatejescu@pm.me>
2026-01-17 19:11:34 +0200

    Add the login card

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
-import OnlineCard from "./components/OnlineCard.jsx";
+import LoginCard from "./components/LoginCard.jsx";
+// import OnlineCard from "./components/OnlineCard.jsx";
 import Copyright from "./components/Copyright.jsx";
 import {Box, Container} from "@mui/material";
+// import RegisterCard from "./components/RegisterCard.jsx";
 
 
 export default function App() {
@@ -9,7 +11,9 @@ export default function App() {
   return (
       <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
           <Container sx={{ display: "flex", flexDirection: "column", justifyContent: "center", flex: 1 }}>
-              <OnlineCard />
+              <LoginCard />
+              {/*<RegisterCard />*/}
+              {/*<OnlineCard /> */}
           </Container>
 
           <Copyright />
---
+++ src/components/LoginCard.jsx
@@ -0,0 +1,87 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import MuiCard from '@mui/material/Card';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+
+
+const Card = styled(MuiCard)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignSelf: 'center',
+    width: '100%',
+    padding: theme.spacing(4),
+    gap: theme.spacing(2),
+    boxShadow:
+        'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+    [theme.breakpoints.up('sm')]: {
+        width: '450px',
+    },
+    ...theme.applyStyles('dark', {
+        boxShadow:
+            'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+    }),
+}));
+
+export default function LoginCard() {
+    return (
+        <Card variant="outlined">
+            <Typography
+                component="h1"
+                variant="h4"
+                sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
+            >
+                Sign in
+            </Typography>
+
+            <Box
+                component="form"
+                // onSubmit={handleSubmit}
+                noValidate
+                sx={{ display: 'flex', flexDirection: 'column', width: '100%', gap: 2 }}
+            >
+                <FormControl>
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <TextField
+                        // error={emailError}
+                        // helperText={emailErrorMessage}
+                        id="email"
+                        type="email"
+                        name="email"
+                        placeholder="your@email.com"
+                        autoComplete="email"
+                        autoFocus
+                        required
+                        fullWidth
+                        variant="outlined"
+                        // color={emailError ? 'error' : 'primary'}
+                    />
+                </FormControl>
+                <FormControl>
+                    <FormLabel htmlFor="password">Password</FormLabel>
+                    <TextField
+                        // error={passwordError}
+                        // helperText={passwordErrorMessage}
+                        name="password"
+                        placeholder="••••••"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        autoFocus
+                        required
+                        fullWidth
+                        variant="outlined"
+                        // color={passwordError ? 'error' : 'primary'}
+                    />
+                </FormControl>
+
+                <Button type="submit" fullWidth variant="contained" /*onClick={validateInputs}*/>
+                    Sign in
+                </Button>
+            </Box>
+        </Card>
+    )
+}
