c1ba398caa3de2c62484f32838290222b990dc0d
Mihai Gătejescu <mgatejescu@pm.me>
2026-01-19 10:35:10 +0200

    Add the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import './App.css'
 import LoginCard from "./components/LoginCard.jsx";
-// import OnlineCard from "./components/OnlineCard.jsx";
+import OnlineCard from "./components/OnlineCard.jsx";
 import Copyright from "./components/Copyright.jsx";
 import {Box, Container} from "@mui/material";
-// import RegisterCard from "./components/RegisterCard.jsx";
+import {Route, Routes} from "react-router";
+import RegisterCard from "./components/RegisterCard.jsx";
 
 
 export default function App() {
 
-  return (
-      <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-          <Container sx={{ display: "flex", flexDirection: "column", justifyContent: "center", flex: 1 }}>
-              <LoginCard />
-              {/*<RegisterCard />*/}
-              {/*<OnlineCard /> */}
-          </Container>
+    return (
+        <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+            <Container sx={{ display: "flex", flexDirection: "column", justifyContent: "center", flex: 1 }}>
+                <Routes>
+                    <Route path="/" element={<OnlineCard />} />
+                    <Route path="/register" element={<RegisterCard />} />
+                    <Route path="/login" element={<LoginCard />} />
+                </Routes>
+            </Container>
 
-          <Copyright />
-      </Box>
+            <Copyright />
+        </Box>
   )
 }
--- src/components/OnlineCard.jsx
+++ src/components/OnlineCard.jsx
@@ -1,12 +1,20 @@
 import {Button, Stack} from "@mui/material";
+import {useNavigate} from "react-router";
 
 
 export default function OnlineCard() {
 
+    const navigate = useNavigate();
+
     return (
         <Stack direction="column" spacing={0.5}>
-            <Button variant="contained">Login</Button>
-            <Button variant="contained">Register</Button>
+            <Button variant="contained" onClick={() => navigate("/login")}>
+                Login
+            </Button>
+
+            <Button variant="contained" onClick={() => navigate("/register")}>
+                Register
+            </Button>
         </Stack>
     )
 }
