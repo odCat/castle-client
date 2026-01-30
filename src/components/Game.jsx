63103b10c1b2e3d239c5eaff8eecd1c275d57cdd
Mihai Gătejescu <mgatejescu@pm.me>
2026-01-30 22:58:28 +0200

    Click to move

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -8,30 +8,70 @@ export default function Game() {
 
     const { gameId } = useParams();
     const chessRef = useRef(new Chess());
-    const chess = chessRef.current;
+    const game = chessRef.current;
 
-    const [chessPosition, setChessPosition] = useState(chess.fen());
+    const [chessPosition, setChessPosition] = useState(game.fen());
+    const [squareOptions, setSquareOptions] = useState({});
+    const [possibleMoves, setPossibleMoves] = useState([]);
 
     function onPieceDrop({ sourceSquare, targetSquare }) {
         if (!targetSquare)
             return false;
-
         try {
-            console.log("Log: " + sourceSquare + " " + targetSquare);
-            chess.move({ from: sourceSquare, to: targetSquare  });
-            setChessPosition(chess.fen());
+            game.move({ from: sourceSquare, to: targetSquare  });
+            setChessPosition(game.fen());
+            setSquareOptions({});
+            setPossibleMoves([]);
             return true;
         } catch {
-            console.log("Illegal move!")
             return false;
         }
     }
 
     function onSquareClick({ square, piece }) {
-        if (piece !== null) {
-            console.log("Piece on " + square + ": " + piece.pieceType);
-        } else
-            console.log("Empty square");
+
+        if (!piece && possibleMoves) {
+            for (const move of possibleMoves) {
+                if (square === move.to) {
+                    game.move({from: move.from, to: move.to})
+                    setChessPosition(game.fen());
+                }
+            }
+            setSquareOptions({});
+            setPossibleMoves([]);
+            return;
+        }
+
+        const moves = game.moves({ square: square, verbose: true });
+
+        if (piece === null || moves.length === 0) {
+            setSquareOptions({});
+            return;
+        }
+
+        setPossibleMoves(moves);
+        const updatedSquares = {};
+        for (const move of moves) {
+            const color = getSquareColor(move.to) === "dark" ? "rgb(60,97,134)" : "rgb(168,170,178)";
+
+            updatedSquares[move.to] = {
+                background:
+                    game.get(move.to) &&
+                    game.get(move.to)?.color !== game.get(square)?.color
+                        ? "radial-gradient(circle, " + color + " 85%, transparent 85%)" // larger circle for capturing
+                        : "radial-gradient(circle, " + color + " 20%, transparent 20%)", // smaller circle for moving
+                borderRadius: '50%',
+            };
+        }
+        updatedSquares[square] = { boxShadow: 'inset 0px 0px 0px 1px black' };
+        setSquareOptions(updatedSquares);
+    }
+
+    function getSquareColor(square) {
+        const rank = parseInt(square[1]) - 1;
+        const file = square[0].charCodeAt(0) - "a".charCodeAt(0);
+
+        return (rank + file) % 2 === 0 ? "dark" : "light";
     }
 
     const chessboardOptions = {
@@ -43,15 +83,16 @@ export default function Game() {
         darkSquareStyle: { backgroundColor: "#578cc1" },
 
         draggingPieceGhostStyle: { opacity: 0 },
-        dropSquareStyle: {
-            boxShadow: 'inset 0px 0px 0px 0px black',
-        },
+        dropSquareStyle: { boxShadow: 'inset 0px 0px 0px 0px black', },
+        draggingPieceStyle: { transform: "scale(1)" },
+        dragActivationDistance: 1,
         showAnimations: false,
 
         onPieceDrop,
         onSquareClick,
 
         position: chessPosition,
+        squareStyles: squareOptions,
     };
 
     return (
