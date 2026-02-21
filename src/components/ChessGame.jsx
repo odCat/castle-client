c4179dfd30106ce3ea6d2fbd18f443fc6a0ae8e9
Mihai Gătejescu <mgatejescu@pm.me>
2026-02-21 20:15:33 +0200

    Remove logging

--- src/components/ChessGame.jsx
+++ src/components/ChessGame.jsx
@@ -197,7 +197,6 @@ export default function ChessGame() {
                 setChessPosition(chessGameRef.current.fen());
                 setSquareOptions({});
                 setPromotionMove(null);
-                console.log(chessGameRef.current.fen());
             }}>Undo</Button>
             <Button variant="contained" onClick={() => {
                 chessGameRef.current.load("rnbqkb1r/pP3ppp/5n2/8/2B5/5N2/PPPp1PPP/RNBQ1RK1 w kq - 0 8");
