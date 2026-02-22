de6d0da86960f6c4a18df141342b44c70409cbbf
Mihai Gătejescu <mgatejescu@pm.me>
2026-02-22 21:00:15 +0200

    Use the styled utility to free the return code

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,36 @@ import {Box, Container} from "@mui/material";
 import {Route, Routes} from "react-router";
 import RegisterCard from "./components/RegisterCard.jsx";
 import ChessGame from "./components/ChessGame/ChessGame.jsx";
+import {styled} from "@mui/material/styles";
 
+
+const MainBox = styled(Box)({
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh"
+})
+
+const MainContainer = styled(Container)({
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    flex: 1
+})
 
 export default function App() {
 
     return (
-        <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-            <Container sx={{ display: "flex", flexDirection: "column", justifyContent: "center", flex: 1 }}>
+        <MainBox>
+            <MainContainer>
                 <Routes>
                     <Route path="/" element={<OnlineCard />} />
                     <Route path="/register" element={<RegisterCard />} />
                     <Route path="/login" element={<LoginCard />} />
                     <Route path="/game/:gameId" element={<ChessGame />} />
                 </Routes>
-            </Container>
+            </MainContainer>
 
             <Copyright />
-        </Box>
+        </MainBox>
   )
 }
