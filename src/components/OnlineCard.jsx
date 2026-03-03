ec1892574bc983315666aa2909182662b90120d4
Mihai Gătejescu <mgatejescu@pm.me>
2026-03-03 18:31:32 +0200

    Show the user as guest when not logged in

--- src/components/OnlineCard.jsx
+++ src/components/OnlineCard.jsx
@@ -1,14 +1,22 @@
 import {Button, Stack} from "@mui/material";
 import {useNavigate} from "react-router";
+import {useDispatch} from "react-redux";
+import {visit} from "../store/actions/actions.js";
 
 
 export default function OnlineCard() {
 
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    function continueAsGuest() {
+        dispatch(visit());
+        navigate("/watch");
+    }
 
     return (
         <Stack direction="column" spacing={0.5}>
-            <Button variant="contained" onClick={() => navigate("/watch")}>
+            <Button variant="contained" onClick={continueAsGuest}>
                 Guest
             </Button>
 
