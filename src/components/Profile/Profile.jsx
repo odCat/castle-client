9a3f36fdb511be6d193f341e9b76877635696875
Mihai Gătejescu <mgatejescu@pm.me>
2026-03-16 12:16:57 +0200

    Fix a few warnings in profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -21,6 +21,8 @@ export default function Profile() {
 
     useEffect(() => {
 
+        if (!params.playerId) return;
+
         const getPlayerInfo = async function(id) {
             console.log(`Getting player ${id} info`);
         }
@@ -38,7 +40,7 @@ export default function Profile() {
 
         getPlayerInfo().then();
         getPlayerHistory(params.playerId).then();
-    }, []);
+    }, [params.playerId]);
 
     function goToGame(event) {
         navigate("/games/id/" + event.currentTarget.id);
