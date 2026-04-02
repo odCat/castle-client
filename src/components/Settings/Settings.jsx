e6bf7c4e40cb5d05af857d93738cef00d60c6fbb
Mihai Gătejescu <mgatejescu@pm.me>
2026-04-02 10:06:44 +0300

    Show the deletion prompt

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,10 +1,11 @@
-import {Box} from "@mui/material";
+import {Box, Dialog, DialogActions, DialogContent, DialogTitle} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import Button from "@mui/material/Button";
 import {useDispatch, useSelector} from "react-redux";
 import {logout} from "../../store/actions/actions.js";
 import {useNavigate} from "react-router";
+import {useState} from "react";
 
 
 export default function Settings() {
@@ -12,6 +13,11 @@ export default function Settings() {
     const player = useSelector(store => store.player);
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+
+    function handleClose() {
+        setOpenDeleteDialog(false);
+    }
 
     async function deleteAccount() {
 
@@ -48,9 +54,21 @@ export default function Settings() {
 
             <Typography variant="h5" sx={{ color: "#d73b3e", mt: 10 }}>Delete account</Typography>
             <Divider sx={{width: "100%", my: 2, borderColor: "#424548"}}/>
-            <Button variant="contained" color="error" onClick={deleteAccount}>
+            <Button variant="contained" color="error" onClick={ () => setOpenDeleteDialog(true) }>
                 Delete your account
             </Button>
+
+            <Dialog
+                open={openDeleteDialog}
+                onClose={handleClose}
+            >
+                <DialogTitle>Are you sure you want to delete your account?</DialogTitle>
+                <DialogContent>This action cannot be undone.</DialogContent>
+                <DialogActions>
+                    <Button onClick={deleteAccount} variant="contained" color="error">Delete</Button>
+                    <Button onClick={handleClose} variant="outlined">Cancel</Button>
+                </DialogActions>
+            </Dialog>
         </Box>
     )
 }
