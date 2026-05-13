1438ca454b7c7edae95bfe0d61bb46b78ed5cdd9
Mihai Gătejescu <mgatejescu@pm.me>
2026-05-13 22:15:06 +0300

    Add the home page tests

---
+++ src/tests/home.test.js
@@ -0,0 +1,38 @@
+import { test, expect } from "@playwright/test";
+
+
+test("has buttons", async ({ page }) => {
+    await page.goto("http://localhost:5173/")
+
+    await expect(page).toHaveTitle("chess-client");
+
+    await expect(page.getByRole("button", { name: "guest"})).toBeVisible();
+    await expect(page.getByRole("button", { name: "login"})).toBeVisible();
+    await expect(page.getByRole("button", { name: "register"})).toBeVisible();
+
+    await expect(page.getByText("Copyright © 2026 Mihai Gătejescu")).toBeVisible();
+})
+
+test("proceed as a guest", async ({ page }) => {
+    await page.goto("http://localhost:5173/")
+
+    await page.getByRole("button", { name: "guest"}).click();
+
+    await expect(page).toHaveURL("http://localhost:5173/watch");
+})
+
+test("go to the login page", async ({ page }) => {
+    await page.goto("http://localhost:5173/")
+
+    await page.getByRole("button", { name: "login"}).click();
+
+    await expect(page).toHaveURL("http://localhost:5173/login");
+})
+
+test("go to the register page", async ({ page }) => {
+    await page.goto("http://localhost:5173/")
+
+    await page.getByRole("button", { name: "register"}).click();
+
+    await expect(page).toHaveURL("http://localhost:5173/register");
+})
