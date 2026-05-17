d179817d9ae48b4eb33080a28b519ef76101ffa4
Mihai Gătejescu <mgatejescu@pm.me>
2026-05-17 12:52:18 +0300

    Add the navigation tests to the demo page

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -17,3 +17,21 @@ test("has components", async ({ page }) => {
     await expect(await page.locator("#chessboard-board")).toHaveCount(1);
     await expect(page.getByText(/^Copyright © 202\d Mihai Gătejescu$/ )).toBeVisible();
 })
+
+test("navigate to the play page", async ({ page }) => {
+    await page.getByRole("button", { name: "Play" }).click();
+
+    await expect(page).toHaveURL("http://localhost:5173/play");
+})
+
+test("navigate to the watch page", async ({ page }) => {
+    await page.getByRole("button", { name: "Watch" }).click();
+
+    await expect(page).toHaveURL("http://localhost:5173/watch");
+})
+
+test("navigate to the demo page", async ({ page }) => {
+    await page.getByRole("button", { name: "Demo" }).click();
+
+    await expect(page).toHaveURL("http://localhost:5173/tools/demo");
+})
