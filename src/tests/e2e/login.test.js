0d462df0b9121a0a0cc7546a84409dd27c21f264
Mihai Gătejescu <mgatejescu@pm.me>
2026-05-18 18:36:55 +0300

    Test player can login

--- src/tests/e2e/login.test.js
+++ src/tests/e2e/login.test.js
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import { generatePlayer } from "../helpers/player.js";
 
 
 test.beforeEach( async ({ page }) => {
@@ -14,4 +15,20 @@ test("has components", async ({ page }) => {
     await expect(page.getByRole("button", { name: /^Login$/ })).toBeVisible();
 
     await expect(page.getByText(/^Copyright © 202\d Mihai Gătejescu$/)).toBeVisible();
+})
+
+test("player can login", async ({ page }) => {
+    await page.goto("http://localhost:5173/register")
+    const player = generatePlayer();
+    await page.getByRole('textbox', { name: "User name" }).fill(player.username);
+    await page.getByRole('textbox', { name: "Email" }).fill(player.email);
+    await page.getByRole('textbox', { name: "Password" }).fill(player.password);
+    await page.getByRole("button", { name: /^Register$/ }).click();
+    await expect(page).toHaveURL("http://localhost:5173/login");
+
+    await page.getByRole('textbox', { name: /^Email\/Username$/ }).fill(player.username);
+    await page.getByRole('textbox', { name: /^Password$/ }).fill(player.password);
+    await page.getByRole("button", { name: /^Login$/ }).click();
+
+    await expect(page).toHaveURL("http://localhost:5173/play");
 })
